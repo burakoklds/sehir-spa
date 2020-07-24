5424829eef1fc5dab541f8c2ad0ef85885fe332c
burak <45562265+burakoklds@users.noreply.github.com>
2020-07-24 17:01:27 +0300

    CityServisiVeImplementasyonu

--- src/app/city/city.component.ts
+++ src/app/city/city.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { City } from 'src/app/models/city';
+import  { CityService } from 'src/app/services/city.service';
 
 @Component({
   selector: 'app-city',
   templateUrl: './city.component.html',
-  styleUrls: ['./city.component.css']
+  styleUrls: ['./city.component.css'],
+  providers:[CityService]
 })
 export class CityComponent implements OnInit {
 
-  constructor() { }
-  cities:City[]
-  
-  ngOnInit() {
+  constructor(private cityService:CityService) { }
+  cities:City[];
 
+  ngOnInit() {
+    this.cityService.getCities().subscribe(data =>{
+      this.cities = data;
+    });
   }
 
 }
