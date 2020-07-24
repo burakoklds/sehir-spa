2fcacde8880f6a004438a86f116a6bb18a5845c2
burak <45562265+burakoklds@users.noreply.github.com>
2020-07-24 18:17:26 +0300

    CityDetailComponenti

--- src/app/services/city.service.ts
+++ src/app/services/city.service.ts
@@ -2,16 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { City } from '../models/city';
+import { Photo } from '../models/photo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CityService {
 
-constructor(private httpClient:HttpClient) { }
-  path="http://localhost:50726/api/";
+  constructor(private httpClient: HttpClient) { }
+  path = "http://localhost:50726/api/";
 
-  getCities():Observable<City[]>{
-    return this.httpClient.get<City[]>(this.path+"cities");
+  getCities(): Observable<City[]> {
+    return this.httpClient.get<City[]>(this.path + "cities");
+  }
+
+  getCityById(cityId): Observable<City> {
+    return this.httpClient.get<City>(this.path + "cities/detail/?id=" + cityId)
+  }
+
+  getPhotosByCity(cityId): Observable<Photo[]> {
+    return this.httpClient.get<Photo[]>(this.path + "cities/photos/?cityId=" + cityId);
   }
 }
